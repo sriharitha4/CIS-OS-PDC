408438aa6d164fbec0c69b225d8669d0952f4952
Nathan Morris <nmorrisk@umich.edu>
2024-07-12 11:58:21 -0400

    Restructured

--- PDCPackage/setup.py
+++ PDCPackage/setup.py
@@ -4,16 +4,11 @@ def read_requirements(file):
     with open(file) as f:
         return f.read().splitlines()
 
-def read_file(file):
-   with open(file) as f:
-        return f.read()
-    
 requirements = read_requirements("requirements.txt")
 
 setup(
-    name = 'pdclib',
+    name = 'PDC',
     url = 'https://github.com/sriharitha4/CIS-OS-PDC',
     packages = find_packages(exclude=["test"]),
-    install_requires = requirements,
-    py_modules=[ "Locality", "Concurrency", "Asynchronousity", "Performance" ]
+    install_requires = requirements
 )
